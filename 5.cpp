ba79f97655eee4224a07e21d9625827804840368
Lawrence <lquarshie29@gmail.com>
2023-07-11 01:16:40 -0400

    Added a C++ file which demonstrates the use of \n in C++ for a new line.

---
+++ 5.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Institute of Radio Engineers \n";
+    cout << "American Institute of Electrical Engineers";
+    return 0;
+}
