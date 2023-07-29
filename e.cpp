7837f7d4fd73a69958648bf33ea6fe46167100be
Lawrence <lquarshie29@gmail.com>
2023-07-29 03:00:31 -0400

    Added C++ files which demonstrates multi-line comment.

---
+++ e.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*The code below will print the words Hello World!
+    to the screen, and it is amazing */
+
+    cout << "Hello World!";
+    return 0;
+}
