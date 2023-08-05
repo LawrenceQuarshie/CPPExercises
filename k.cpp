21c70eb66f77f7fe422041838c9e0df4ea523abd
Lawrence <lquarshie29@gmail.com>
2023-08-05 17:46:00 -0400

    Added C++ files that demonstrates the concatenation of strings and integers.

---
+++ k.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myAge = 30;
+    int myYear = 1993;
+    cout << "I was born in " << myYear << " and I am " << myAge << " years old.";
+    return 0;
+}
