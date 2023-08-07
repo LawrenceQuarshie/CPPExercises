740543f3b4cea37fcd7347f61f8fa273b67519cb
Lawrence <lquarshie29@gmail.com>
2023-08-07 01:10:37 -0400

    Added C++ files which demonstrates addition operation in C++.

---
+++ m.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    // operation
+    int c = a / b;
+
+    // output
+    cout << c;
+    return 0;
+}
