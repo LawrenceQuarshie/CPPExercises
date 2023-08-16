780b869f2c3ebc0640b692301af69217bf0887c2
Lawrence <lquarshie29@gmail.com>
2023-08-15 20:54:24 -0400

    Added C++ files which demonstrates the use of const variables in C++.

---
+++ r.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int minutesPerSecond = 60;
+    const float PI = 3.14;
+    cout << minutesPerSecond << "\n";
+    cout << PI;
+    return 0;
+}
