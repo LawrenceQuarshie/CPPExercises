43bd962f37311aec68b10a30cd78cea2418469bc
Lawrence <lquarshie29@gmail.com>
2023-08-21 00:29:11 -0400

    Added C++ files that demonstrates the use of inputs and outputs in C++.

---
+++ t.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int flux;
+    cout << "Enter a value for the magnetic flux ";
+    cin >> flux;
+    cout << "The value of the magnetic flux is " << flux;
+    return 0;
+}
