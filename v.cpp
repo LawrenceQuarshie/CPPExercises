90ea93c72504e073d3c8284d3dca044360081c15
Lawrence <lquarshie29@gmail.com>
2023-08-26 03:29:45 -0400

    Added C++ files that demonstrates the various data types in C++.

---
+++ v.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int myInt = 5;
+    float myFloat = 5.99;
+    double myDouble = 9.98;
+    char myChar = 'D';
+    bool myBoolean = true;
+    string myString = "Hello";
+
+    /* output */
+    cout << "int: " << myInt << "\n";
+    cout << "float: " << myFloat << "\n";
+    cout << "double: " << myDouble << "\n";
+    cout << "char: " << myChar << "\n";
+    cout << "bool: " << myBoolean << "\n";
+    cout << "string: " << myString;
+    return 0;
+}
